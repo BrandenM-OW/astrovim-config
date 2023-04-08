168adee12e532ca11d90e25cc28215918edbc1be
BrandenM-OW <bmorin@openwater.com>
2023-04-08 19:31:31 -0400

    Moved to community copilot conf and moved mappings for colemak.

--- mappings.lua
+++ mappings.lua
@@ -8,10 +8,18 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    -- remap j and k to n and e respectively
+    ["j"] = { "e" },
+    ["k"] = { "n" },
+    ["n"] = { "j" },
+    ["e"] = { "k" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },
