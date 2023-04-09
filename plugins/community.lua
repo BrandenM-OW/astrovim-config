bf5c78e4363694a7e69f838b9bff21ed37176c1d
BrandenM-OW <bmorin@openwater.com>
2023-04-08 20:24:52 -0400

    Moved on to the community leap config and new theme.

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,6 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
 }
